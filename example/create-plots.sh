5dd3c2dff922447592f5c16f29c8ee609301d889
Egor Vdovenko <egor.vdovenko@natlex.ru>
2023-05-03 20:31:15 +0300

    feat: ellipse

--- example/create-plots.sh
+++ example/create-plots.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/env bash
 
-# fetch yearly sample data from https://datahub.io/core/global-temp
-curl -L https://datahub.io/core/global-temp/r/annual.csv > global-temp-data.csv
-
-# generate the plot
-docker run --rm -v $(pwd):/work remuslazar/gnuplot global-temp.gp
-
 # generate a plot with multiple data sets
-docker run --rm -v $(pwd):/work remuslazar/gnuplot global-temp-multi.gp
+docker run --rm -v $(pwd):/work remuslazar/gnuplot ellipse.gp
+docker run --rm -v $(pwd):/work remuslazar/gnuplot hyperbola.gp
+docker run --rm -v $(pwd):/work remuslazar/gnuplot parabola.gp
---
+++ example/ellipse.gp
@@ -0,0 +1,21 @@
+set parametric
+
+set terminal png
+set output 'ellipse.png'
+
+set xlabel 'X'
+set ylabel 'Y'
+
+set size ratio 1
+set xrange [-5:5]
+set yrange [-5:5]
+
+a = 2.0
+b = 3.0
+
+# Уравнение эллипса в декартовых координатах
+x(t) = a*cos(t)
+y(t) = b*sin(t)
+
+# Построение эллипса
+plot x(t), y(t) with lines lw 2 lc rgb 'red' title 'Эллипс'
